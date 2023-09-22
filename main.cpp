88afcd9d22b9fb2c09636aff86a838a9409e3744
ryancart <ryancartwright@mac.com>
2023-09-21 19:40:23 -0500

    should have started sooner

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <iomanip>
+using namespace std;
+
+void fillArray (int lowestValue, int highestValue, int range, int num, int[][2]) {
+    if (num >= lowestValue && num <= highestValue) {
+        data[range - num][1]++;
+    }
+    else {
+        cout << "Error: value" << num << "is out of range";
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int num;
+
+    cin >> num;
+    int lowestValue = num;
+
+    cin >> num;
+    int highestValue = num;
+
+    int range = 1;
+    range = highestValue - lowestValue;
+
+    int data[range][2];
+    for (int y = 0; y < range; ++y) {
+        data[y][0] = lowestValue + y;
+        data[y][1] = 0;
+    }
+
+
+    cin >> num;
+    while(cin) {
+        fillArray(lowestValue, highestValue, range, num, data);
+    }
+
+
+
+
+
     return 0;
 }
 
-def array getData() {
 
-}
+
